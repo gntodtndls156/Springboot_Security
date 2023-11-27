b091449594db1c4d4a0637b092b0c098a654f4ed
gntodtndls156 <gntodtndls156@gmail.com>
2023-11-27 14:28:57 +0900

    First Project

---
+++ src/test/java/com/github/gntodtndls156/ExampleSecurity/ExampleSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gntodtndls156.ExampleSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
